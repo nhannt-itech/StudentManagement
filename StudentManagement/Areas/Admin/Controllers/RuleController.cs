fc1f312dbd274e60c678be4d6b886f04aa5c03ad
nhannt-itech <67116951+nhannt-itech@users.noreply.github.com>
2021-01-03 14:48:43 +0700

    rule controller

---
+++ StudentManagement/Areas/Admin/Controllers/RuleController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StudentManagement.DataAccess.Repository.IRepository;
+using StudentManagement.Models;
+
+namespace StudentManagement.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RuleController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public RuleController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Update(string id)
+        {
+            var ruleObj = _unitOfWork.Rule.Get(id);
+            return View(ruleObj);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Rule rule)
+        {
+            _unitOfWork.Rule.Update(rule);
+            _unitOfWork.Save();
+            return View("Index");
+        }
+
+        #region API CALL
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Rule.GetAll();
+            return Json(new { data = allObj });
+        }
+        #endregion
+    }
+}
---
+++ StudentManagement/wwwroot/js/rule.js
@@ -0,0 +1,29 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/Rule/GetAll"
+        },
+        "columns": [
+            { "data": "name" },
+            { "data": "min" },
+            { "data": "max" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                            <div class="text-center">
+                                <a href="/Admin/Rule/Update/${data}" class="btn btn-success bg-gradient-success text-white" style="cursor:pointer">
+                                    <i class="fas fa-edit"></i>
+                                </a>
+                            </div>
+                            `;
+                }, "width": "15%"
+            }
+        ]
+    });
+}
